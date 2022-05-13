57d97f94ae8ee8e51c7f9d5c68d49926a5440e3e
luong khac nam <luongkhacnam222@gmail.com>
2022-05-13 20:02:01 +0700

    deploy heroku

--- index.js
+++ index.js
@@ -1,37 +1,63 @@
-const express = require('express');
-const res = require('express/lib/response');
-const { Todo } = require('./database');
+const express = require("express")
+const res = require("express/lib/response")
+const { Todo } = require("./database")
 
-const app = express();
-const PORT = process.env.PORT || 8000;
+const app = express()
+const PORT = 8000
 
-app.use(express.json());
+app.use(express.json())
 
-app.post('/todo', async (req, res) => {
+app.post("/todo", async (req, res) => {
     try {
-        const {name} = req.body;
+        const { name } = req.body
 
-        if (name == '') throw Error('Not empty'); 
+        if (name == "") throw Error("Not empty")
 
-        await Todo.create({ name, complete: false });
+        await Todo.create({ name, complete: false })
 
-        res.status(200).json({message: 'Create success'});
+        res.status(200).json({ message: "Create success" })
     } catch (error) {
-        res.status(400).json({message: error.message});
+        res.status(400).json({ message: error.message })
     }
-});
+})
 
-app.get('/todos', async (req, res) => {
+app.get("/todos", async (req, res) => {
     try {
-        const todos = await Todo.find({});
-        res.status(200).json({todos: todos});
+        const todos = await Todo.find({})
+        res.status(200).json({ todos: todos })
     } catch (error) {
-        res.status(400).json({message: error.message});
+        res.status(400).json({ message: error.message })
     }
-});
+})
 
 // Update by id: findOneAndUpdate// Delete by id: deleteOne
+app.put("/todo/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name } = req.body
+
+        await Todo.findByIdAndUpdate(id, { name: name })
+
+        const todos = await Todo.find({})
+        res.status(200).json({ todos: todos })
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+app.delete("/todo/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+
+        await Todo.findByIdAndRemove(id)
+
+        const todos = await Todo.find({})
+        res.status(200).json({ todos: todos })
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
 
 app.listen(PORT, () => {
-    console.log('Server is listening on :', PORT);
-});
+    console.log("Server is listening on :", PORT)
+})
